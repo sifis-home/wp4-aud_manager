0abd925b28716d01a2c2a8f6e1a148787d9ea472
Otto Waltari <otto.waltari@f-secure.com>
2023-05-25 14:00:51 +0300

    Commit for debugging sll_pkttype in ConnEntries

--- aud_sensor/aud_conn.py
+++ aud_sensor/aud_conn.py
@@ -102,12 +102,14 @@ class ConnEntry():
     def __init__(self, key, l3hdr, l4hdr): #ip_ver, t0):
         self.key = key
 
+        self.direction = l3hdr.direction
         if l3hdr.direction == pr.socket.PACKET_HOST:
             self.acl_direction = "to"
             self.acl_addr = l3hdr.src
             self.local_ip = l3hdr.dst
 
-        elif l3hdr.direction == pr.socket.PACKET_OUTGOING:
+        #elif l3hdr.direction == pr.socket.PACKET_OUTGOING:
+        else:
             self.acl_direction = "from"
             self.acl_addr = l3hdr.dst
             self.local_ip = l3hdr.src
@@ -128,7 +130,7 @@ class ConnEntry():
 
 
     def __str__(self):
-        return str(self.key)+", active="+str(self.active())
+        return str(self.key)+", direction="+str(self.direction)+", active="+str(self.active())
 
     def active(self):
         return (self.last_updated > (time.time_ns() - (self.timeout * 1000000000)))
